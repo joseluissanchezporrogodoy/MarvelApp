10805ae1545d563b00d0946ccc7a442bada2389a
JoseLuisSPG <joseluis.sanchezporro.godoy@gmail.com>
2018-01-06 12:58:09 +0100

    Character details I (image and description)

--- src/sections/characters/CharacterDetail.js
+++ src/sections/characters/CharacterDetail.js
@@ -6,12 +6,32 @@ import { connect } from 'react-redux'
 class CharacterDetail extends Component {
   render() {
       const { character } = this.props
-      return(
-          <View>
-              <Text> Estoy en la segunda pantalla</Text>
+
+
+
+      const image = character.thumbnail && !character.thumbnail.path.endsWith('image_not_available') ?
+          { uri: `${character.thumbnail.path.replace('http', 'https')}/landscape_large.${character.thumbnail.extension}` } :
+          require('marvel_app/src/resources/unknown.jpg')
+
+      const description = character && character.description ? character.description : 'No Description'
+      const comics = character && character.comics && character.comics.items ? character.comics.items : null
+      const events = character && character.events && character.events.items ? character.events.items : null
+      const series = character && character.series && character.series.items ? character.series.items : null
+      const stories = character && character.stories && character.stories.items ? character.stories.items : null
+      console.log(image)
+      return (
+          <View style={styles.container}>
+              <Image source={image} style={styles.image} resizeMode={'cover'} />
+              <ScrollView>
+                  <Text style={styles.description}>{description}</Text>
+                  <View style={[styles.container, {padding: 8}]}>
+
+                  </View>
+              </ScrollView>
           </View>
       )
   }
+
 
 }
 
@@ -22,3 +42,35 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, null)(CharacterDetail)
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    description: {
+        flex: 1,
+        fontSize: 15,
+        fontWeight: '600',
+        color: 'black',
+        alignItems: 'center',
+        padding: 6
+    },
+
+    image: {
+        width: '100%',
+        height: 180,
+    },
+
+    buttonContainer: {
+        margin: 20,
+    },
+});
